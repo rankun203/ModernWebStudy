712f24dc9493f2744acae9a97f9b2e952283f675
rankun203 <rankun203@gmail.com>
2014-09-28 11:34:17 +0800

    update 0928 exercises

--- c/0928/if.c
+++ c/0928/if.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <conio.h>
 
+/**
+ * @author rankun203@gmail.com
+ * Convert a number to Season Name with if statements.
+ */
+
+/**
+ * check if season number is correct.
+ */
+int check_season(int s);
+
 int main()
 {
 	int season;
 	printf("Input a season number: ");
 	scanf("%d", &season);
 	
-	printf("The season is: ");
-	
-	if(1 <= season && season <= 3)
-		printf("Spring");
-	else if (4 <= season && season <= 6)
-		printf("Summer");
-	else if (7 <= season && season <= 9)
-		printf("Fall");
-	else
-		printf("Winter");
+	if (check_season(season) == 0) {	
+		printf("The season is: ");
+		if(1 <= season && season <= 3)
+			printf("Spring");
+		else if (4 <= season && season <= 6)
+			printf("Summer");
+		else if (7 <= season && season <= 9)
+			printf("Fall");
+		else
+			printf("Winter");
+	} else {
+		printf("Input Error: Input should be an Integer between 1 and 12.");
+	}
 	
 	getch();
 	return 0;
 }
+
+int check_season(int s)
+{
+	if(s < 1 || s > 12)
+		return -1;
+	return 0;
+}
+
--- c/0928/switch.c
+++ c/0928/switch.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 
+/**
+ * @author rankun203@gmail.com
+ * Convert a number to Season Name with if statements.
+ */
+
+/**
+ * check if season number is correct.
+ */
+int check_season(int s);
+
 int main()
 {
 	int season;
 	printf("Input a season number: ");
 	scanf("%d", &season);
-	
-	printf("The season is: ");
-	
-	switch(season){
-		case 1:
-		case 2:
-		case 3: printf("Spring");	break;
-		case 4:
-		case 5:
-		case 6: printf("Summer");	break;
-		case 7:
-		case 8:
-		case 9: printf("Autumn");	break;
-		case 10:
-		case 11:
-		case 12: printf("Winter");	break;
+	if (check_season(season) == 0) {
+		printf("The season is: ");
+		switch(season){
+			case 1:
+			case 2:
+			case 3: printf("Spring");	break;
+			case 4:
+			case 5:
+			case 6: printf("Summer");	break;
+			case 7:
+			case 8:
+			case 9: printf("Autumn");	break;
+			case 10:
+			case 11:
+			case 12: printf("Winter");	break;
+		}
+	} else {
+		printf("Input Error: Input should be an Integer between 1 and 12.");
 	}
 	
 	getch();
 
 	return 0;
 }
+
+int check_season(int s)
+{
+	if(s < 1 || s > 12)
+		return -1;
+	return 0;
+}
+
