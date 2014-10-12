a8ccb5df61ba938f1a5c533e97d4bf3b8e76405c
Kun <rankun203@gmail.com>
2014-10-12 17:23:38 +0800

    update

--- c/1008/factorial/main.c
+++ c/1008/factorial/main.c
@@ -4,18 +4,18 @@ int main()
 {
 	long i, n, f;
 	printf("Please input a number: ");
-	scanf("%d", &n);
+	scanf("%ld", &n);
 
 	f = n;
 	for(i=n-1; i>0; i--)
 	{
-		printf("%d", i+1);
+		printf("%ld", i+1);
 		f = f * i;
 		if(i!=1)
 			printf("x");
 	}
 
-	printf("x1=%d\n", f);
+	printf("x1=%ld\n", f);
 	
 	return 0;
 }
