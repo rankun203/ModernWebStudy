342974b4789d4143eaf19115ab010e4d5c39df77
Kun <rankun203@gmail.com>
2014-10-30 06:51:53 +0800

    update

---
+++ c/1026/strcpy/strcpy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  strcpy
+//
+//  Created by Kun on 10/26/14.
+//  Copyright (c) 2014 Kun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ c/1028/1028/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1028
+//
+//  Created by Kun on 10/28/14.
+//  Copyright (c) 2014 Kun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
