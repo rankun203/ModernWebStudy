a2911d2fd71cd4aac39742fb437ffe7ee0dfb57d
Kun <rankun203@gmail.com>
2014-11-03 15:03:59 +0800

    udpate

---
+++ c/Others/PointerArray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void test(int** p)
+{
+
+}
+
+int main()
+{
+  int arr[] = {30, 450, 14, 5};
+  test(&arr);
+  return 0;
+}
---
+++ express.js/yotest1/js/modules/module.js
@@ -0,0 +1,18 @@
+/**
+ * Module module
+ *
+ * @author John Doe
+ * @date 2014-10-30
+ */
+
+define([], function () {
+
+	// Strict mode to prevent sloppy JS
+	'use strict';
+
+	// Public API
+	return {
+		
+	};
+
+});
