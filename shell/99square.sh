4b26193884d9e1670b000780e3c9eab27954b638
rankun203 <rankun203@gmail.com>
2015-06-30 21:09:07 +0800

    ADD: Nine-nine square table

---
+++ shell/99square.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for i in 1 2 3 4 5 6 7 8 9; do
+    for j in 1 2 3 4 5 6 7 8 9; do
+        if [ ${j} -le ${i} ]; then
+            echo -n $((${i}*${j}))"     "
+        fi
+    done
+    echo ""
+done
