8106ead29692e701f9c41b98efacc867aa5dfdc5
rankun203 <rankun203@gmail.com>
2015-07-01 19:12:38 +0800

    ADD: Find the prime number.

---
+++ java/src/org/rankun/beforeTest/PrimeNumber.java
@@ -0,0 +1,25 @@
+package org.rankun.beforeTest;
+
+/**
+ * Created on 7/1/15.
+ *
+ * @author rankun203
+ */
+public class PrimeNumber {
+    public static void main(String... args) {
+        int max = 100_000_000;
+
+        for (int n = 2; n < max; n++) {
+
+            boolean isPrime = true;
+            for (int test = 2; test < n; test++) {
+                // 只要发现 n % test == 0 (一旦被整除), 说明不是素数, isPrime 置为 false
+                isPrime &= !(n % test == 0);
+                if (isPrime) break;
+            }
+
+            if (isPrime)
+                System.out.println(n + " is a prime number");
+        }
+    }
+}
