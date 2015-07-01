fc5387764f6f69891ca08412cb350b00d9c4fe5a
rankun203 <rankun203@gmail.com>
2015-07-01 19:21:24 +0800

    ADD: Find the prime number - in Parallel.

--- java/src/org/rankun/beforeTest/PrimeNumberJava8.java
+++ java/src/org/rankun/beforeTest/PrimeNumberJava8.java
@@ -1,18 +1,25 @@
 package org.rankun.beforeTest;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created on 7/1/15.
  *
  * @author rankun203
  */
-public class PrimeNumber {
+public class PrimeNumberJava8 {
     public static void main(String... args) {
         int max = 100_000;
 
+        List<Integer> intList = new LinkedList<>();
+        for (int i = 2; i < max; i++) {
+            intList.add(i);
+        }
+
         long start = System.currentTimeMillis();
 
-        for (int n = 2; n < max; n++) {
-
+        intList.parallelStream().forEach(n -> {
             boolean isPrime = true;
             for (int test = 2; test < n; test++) {
                 // 只要发现 n % test == 0 (一旦被整除), 说明不是素数, isPrime 置为 false
@@ -22,7 +29,7 @@ public class PrimeNumber {
 
             if (isPrime)
                 System.out.println(n + " is a prime number");
-        }
+        });
 
         System.out.println("Costs: " + (System.currentTimeMillis() - start) + "ms");
     }
